057c5e062d21f36fcb793fe23e54d0f688294e12
Pai <b8350580@gmail.com>
2023-04-06 10:28:20 +0800

    generate password

--- Develop/script.js
+++ Develop/script.js
@@ -29,4 +29,51 @@ function writePassword() {
     return writePassword();
   }
 
-  
+  // generate character set before we create the pw
+  let charsets = "";
+
+  if(isSymbols){
+    charsets = charsets + "!@#$%^&*()";
+  }
+
+  if (isLowercase) {
+    charsets = charsets + "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (isUppercase) {
+    charsets = charsets + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (isNumber) {
+    charsets = charsets + "1234567890";
+  }
+
+  // generate pw based on criteria selected
+
+
+  const password = generatePassword(PasswordLength, charsets);
+
+  // insert pw to textarea
+  textareaEl.value = password;
+}
+
+
+
+
+function generatePassword(length, charsets){
+
+  let password = "";
+
+  // get random character for 'pwLength' times
+  for (let x = 0; x < length; x++) {
+    // get a random character based on charsets
+    const randomIndex = Math.floor(Math.random() * charsets.length)
+
+    password = password + charsets[randomIndex];
+
+  }
+  return password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
